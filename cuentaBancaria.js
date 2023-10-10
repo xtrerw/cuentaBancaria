7248c56965ad6bf0ed16e33cbbe7a9c8bf592b54
wei <miguelxu124@.gmail.com>
2023-10-10 22:32:46 +0200

    fk

--- cuentaBancaria.js
+++ cuentaBancaria.js
@@ -1,8 +1,9 @@
 var dom=document.querySelector('h2');
+var btn=document.getElementById('ingresa');
 let cliente1={
     nombre: 'Ismael',
     apellido: 'Mor Mor',
-    dni: 'A1111111A',
+    dni: '1',
     cantidadDeDinero: 3000,
 };
 let cliente2={
@@ -21,15 +22,21 @@ let cliente3={
 let dni = prompt('Introduce DNI de usted, porfavor');
 switch (dni) {
     case cliente1.dni:
-        dom.innerHTML+=cliente1.nombre;
+        dom.innerHTML+=cliente1.nombre+'<br>'+cliente1.apellido+'<br>'+cliente1.cantidadDeDinero;
         break;
     case cliente2.dni:
-        dom.innerHTML+=cliente2.nombre;
+        dom.innerHTML+=cliente2.nombre+'<br>'+cliente2.apellido+'<br>'+cliente2.cantidadDeDinero;
         break
     case cliente3.dni:
-        dom.innerHTML+=cliente3.nombre;
+        dom.innerHTML+=cliente3.nombre+'<br>'+cliente3.apellido+'<br>'+cliente3.cantidadDeDinero;
         break
     default:
         dom.innerHTML+="no existe este client";
         break;
 }
+
+function ingresa() {
+    let ingresa = prompt('Introduce cantidad del ingreso');
+    cliente1.cantidadDeDinero -= -ingresa;
+    dom.innerHTML+=cliente1.cantidadDeDinero;
+ };
