ccb4a700db7e14ea947c29bc4448637bf861dfc7
wei <miguelxu124@.gmail.com>
2023-10-10 21:02:31 +0200

    ok

--- cuentaBancaria.js
+++ cuentaBancaria.js
@@ -1,21 +1,23 @@
+var dom=document.querySelector('h2');
 const cliente1={
     nombre: 'Ismael',
-    apeilldo: 'Mor Mor',
+    apellido: 'Mor Mor',
     dni: 'Y2343242H',
     cantidadDeDinero: 3000,
 };
 const cliente2={
     nombre: 'Wei',
-    apeilldo: 'Xu',
+    apellido: 'Xu',
     dni: 'I2342360H',
     cantidadDeDinero: 5000,
 };
 const cliente3={
     nombre: 'Daza',
-    apeilldo: 'Épico',
+    apellido: 'Épico',
     dni: 'P2331123H',
     cantidadDeDinero: 6000,
 };
+
 let dni1 = cliente1['dni'];
 let dni2 = cliente2['dni'];
 let dni3 = cliente3['dni'];
@@ -24,10 +26,14 @@ let nombre1 = cliente1['nombre'];
 let nombre2 = cliente2['nombre'];
 let nombre3 = cliente3['nombre'];
 
-let apeilldo1 = cliente1['apeilldo'];
-let apeilldo2 = cliente2['apeilldo'];
-let apeilldo3 = cliente3['apeilldo'];
+let apellido1 = cliente1['apellido'];
+let apellido2 = cliente2['apellido'];
+let apellido3 = cliente3['apellido'];
 
 let saldo1 = cliente1['cantidadDeDinero'];
 let saldo2 = cliente1['cantidadDeDinero'];
 let saldo3 = cliente1['cantidadDeDinero'];
+
+dom.innerHTML+='nombre: '+nombre1+'<br>apellido: '+apellido1+'<br>DNI: '+dni1;
+dom.innerHTML+='<br>nombre: '+nombre2+'<br>apellido: '+apellido2+'<br>DNI: '+dni2;
+dom.innerHTML+='<br>nombre: '+nombre3+'<br>apellido: '+apellido3+'<br>DNI: '+dni3;
