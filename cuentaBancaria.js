b2c11fa6fa831e1adc67870aba024d83c246973d
wei <miguelxu124@.gmail.com>
2023-10-10 20:42:02 +0200

    información de usuario

---
+++ cuentaBancaria.js
@@ -0,0 +1,33 @@
+const cliente1={
+    nombre: 'Ismael',
+    apeilldo: 'Mor Mor',
+    dni: 'Y2343242H',
+    cantidadDeDinero: 3000,
+};
+const cliente2={
+    nombre: 'Wei',
+    apeilldo: 'Xu',
+    dni: 'I2342360H',
+    cantidadDeDinero: 5000,
+};
+const cliente3={
+    nombre: 'Daza',
+    apeilldo: 'Épico',
+    dni: 'P2331123H',
+    cantidadDeDinero: 6000,
+};
+let dni1 = cliente1['dni'];
+let dni2 = cliente2['dni'];
+let dni3 = cliente3['dni'];
+
+let nombre1 = cliente1['nombre'];
+let nombre2 = cliente2['nombre'];
+let nombre3 = cliente3['nombre'];
+
+let apeilldo1 = cliente1['apeilldo'];
+let apeilldo2 = cliente2['apeilldo'];
+let apeilldo3 = cliente3['apeilldo'];
+
+let saldo1 = cliente1['cantidadDeDinero'];
+let saldo2 = cliente1['cantidadDeDinero'];
+let saldo3 = cliente1['cantidadDeDinero'];
