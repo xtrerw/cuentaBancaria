b6b24de7ae5497d45af303428a40fb38af3989e6
wei <miguelxu124@.gmail.com>
2023-10-10 21:41:04 +0200

    comprobar el cliente

--- cuentaBancaria.js
+++ cuentaBancaria.js
@@ -1,39 +1,35 @@
 var dom=document.querySelector('h2');
-const cliente1={
+let cliente1={
     nombre: 'Ismael',
     apellido: 'Mor Mor',
-    dni: 'Y2343242H',
+    dni: 'A1111111A',
     cantidadDeDinero: 3000,
 };
-const cliente2={
+let cliente2={
     nombre: 'Wei',
     apellido: 'Xu',
     dni: 'I2342360H',
     cantidadDeDinero: 5000,
 };
-const cliente3={
+let cliente3={
     nombre: 'Daza',
     apellido: 'Épico',
     dni: 'P2331123H',
     cantidadDeDinero: 6000,
 };
 
-let dni1 = cliente1['dni'];
-let dni2 = cliente2['dni'];
-let dni3 = cliente3['dni'];
-
-let nombre1 = cliente1['nombre'];
-let nombre2 = cliente2['nombre'];
-let nombre3 = cliente3['nombre'];
-
-let apellido1 = cliente1['apellido'];
-let apellido2 = cliente2['apellido'];
-let apellido3 = cliente3['apellido'];
-
-let saldo1 = cliente1['cantidadDeDinero'];
-let saldo2 = cliente1['cantidadDeDinero'];
-let saldo3 = cliente1['cantidadDeDinero'];
-
-dom.innerHTML+='nombre: '+nombre1+'<br>apellido: '+apellido1+'<br>DNI: '+dni1;
-dom.innerHTML+='<br>nombre: '+nombre2+'<br>apellido: '+apellido2+'<br>DNI: '+dni2;
-dom.innerHTML+='<br>nombre: '+nombre3+'<br>apellido: '+apellido3+'<br>DNI: '+dni3;
+let dni = prompt('Introduce DNI de usted, porfavor');
+switch (dni) {
+    case cliente1.dni:
+        dom.innerHTML+=cliente1.nombre;
+        break;
+    case cliente2.dni:
+        dom.innerHTML+=cliente2.nombre;
+        break
+    case cliente3.dni:
+        dom.innerHTML+=cliente3.nombre;
+        break
+    default:
+        dom.innerHTML+="no existe este client";
+        break;
+}
