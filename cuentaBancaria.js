c4507d6374bb02aaca8c69d5283802c2b1b0a99e
wei <miguelxu124@.gmail.com>
2023-10-11 23:03:40 +0200

    modificado muchisimos

--- cuentaBancaria.js
+++ cuentaBancaria.js
@@ -1,5 +1,9 @@
 var dom=document.querySelector('h2');
-var btn1=document.getElementById('ingresa');
+var btn1=document.getElementById('ingresar');
+var btn2=document.getElementById('retirar');
+var btn3=document.getElementById('Lista de clientes');
+var btn4=document.getElementById('Cambio de usuario');
+var btn5=document.getElementById('Agregar cliente');
 let cliente1={
     nombre: 'Ismael',
     apellido: 'Mor Mor',
@@ -21,33 +25,45 @@ let cliente3={
 
 let dni = prompt('Introduce DNI de usted, porfavor');
 var ingreso=0;
+var retiro=0;
+function actualizarCliente(cliente) { 
+    dom.innerHTML=cliente.nombre+'<br>'+cliente.apellido+'<br>'+cliente.cantidadDeDinero;
+    btn1.addEventListener('click',function() { 
+        ingreso = Number(prompt('Introduce cantidad del ingreso'));
+        cliente.cantidadDeDinero += ingreso;
+        dom.innerHTML=cliente.nombre+'<br>'+cliente.apellido+'<br>'+cliente.cantidadDeDinero;
+        })
+    btn2.addEventListener('click',function() {
+        if (cliente.cantidadDeDinero>=0) {
+            retiro=Number(prompt('Introduce el monto del retiro'));
+            if (retiro<= cliente.cantidadDeDinero){
+                cliente.cantidadDeDinero -= retiro;
+                dom.innerHTML=cliente.nombre+'<br>'+cliente.apellido+'<br>'+cliente.cantidadDeDinero;
+            }else if (retiro>cliente.cantidadDeDinero) {
+                dom.innerHTML=cliente.nombre+'<br>'+cliente.apellido+'<br>'+'no se puede retirar más efectivos';
+            }else{
+                dom.innerHTML=cliente.nombre+'<br>'+cliente.apellido+'<br>'+'introduce un número';
+            }
+        }else{
+            dom.innerHTML=cliente.nombre+'<br>'+cliente.apellido+'<br>'+'no se puede retirar más efectivos';
+        }
+    });
+ }
 switch (dni) {
     case cliente1.dni:
-        dom.innerHTML=cliente1.nombre+'<br>'+cliente1.apellido+'<br>'+cliente1.cantidadDeDinero;
-        btn1.addEventListener('click',function() { 
-            ingreso = Number(prompt('Introduce cantidad del ingreso'));
-            cliente1.cantidadDeDinero += ingreso;
-            dom.innerHTML=cliente1.nombre+'<br>'+cliente1.apellido+'<br>'+cliente1.cantidadDeDinero;
-         })
+        actualizarCliente(cliente1);
         break;
     case cliente2.dni:
-        dom.innerHTML+=cliente2.nombre+'<br>'+cliente2.apellido+'<br>'+cliente2.cantidadDeDinero;
-        btn1.addEventListener('click',function() { 
-            ingreso = Number(prompt('Introduce cantidad del ingreso'));
-            cliente2.cantidadDeDinero += ingreso;
-            dom.innerHTML=cliente2.nombre+'<br>'+cliente2.apellido+'<br>'+cliente2.cantidadDeDinero;
-         })
+        actualizarCliente(cliente2);
         break
     case cliente3.dni:
-        dom.innerHTML+=cliente3.nombre+'<br>'+cliente3.apellido+'<br>'+cliente3.cantidadDeDinero;
-        btn1.addEventListener('click',function() { 
-            ingreso = Number(prompt('Introduce cantidad del ingreso'));
-            cliente3.cantidadDeDinero += ingreso;
-            dom.innerHTML=cliente3.nombre+'<br>'+cliente3.apellido+'<br>'+cliente3.cantidadDeDinero;
-         })
+        actualizarCliente(cliente3);
         break
     default:
         dom.innerHTML+="no existe este client";
         break;
 }
 
+btn3.addEventListener('click',function(){
+        dom.innerHTML+="<br>DNI: "+cliente1.dni+"</br>DNI: "+cliente2.dni+"</br>DNI: "+cliente3.dni+"</br>";
+});
